b1cbd9694d83d8482cb8a8667251cd02c78531a3
Somendrasingh05 <singhsomendra05092000@gmail.com>
2022-07-14 23:29:51 +0530

    strings

--- JS/strings.js
+++ JS/strings.js
@@ -7,3 +7,5 @@ console.log(str[4]); // it will print o because o is in 4th index
 console.log(str[6]); // it will print p beacuse p is in 6th index
 
 console.log(str.length);
+i am a disco dancer hehe jindagi mera gana me kisi ka diwana
+to jhumo to nacho to nacho gao mere sang i am a disco dancer
