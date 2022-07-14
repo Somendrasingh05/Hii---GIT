9ba03c832040e457af41eb7c314f40b0a08a9343
Somendrasingh05 <singhsomendra05092000@gmail.com>
2022-07-14 23:26:50 +0530

    strings start

---
+++ JS/strings.js
@@ -0,0 +1,9 @@
+// string is a sequence of characters
+
+var str='Hello pepcoders';
+console.log(str);
+// H -> 0 index , e -> 1 index , l-> 2 index
+console.log(str[4]); // it will print o because o is in 4th index
+console.log(str[6]); // it will print p beacuse p is in 6th index
+
+console.log(str.length);
